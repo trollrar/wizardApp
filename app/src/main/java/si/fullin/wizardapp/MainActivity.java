8bf9016df5b2f4b69549abd792277360973e99ce
aljazerzen <aljaz.erzen@gmail.com>
2019-10-26 21:41:31 +0200

    usb serial

--- app/src/main/java/si/fullin/wizardapp/MainActivity.java
+++ app/src/main/java/si/fullin/wizardapp/MainActivity.java
@@ -1,55 +1,35 @@
 package si.fullin.wizardapp;
 
-import com.hoho.android.usbserial.driver.UsbSerialDriver;
-import com.hoho.android.usbserial.driver.UsbSerialPort;
-import com.hoho.android.usbserial.driver.UsbSerialProber;
-import com.hoho.android.usbserial.util.SerialInputOutputManager;
-import com.physicaloid.lib.Physicaloid;
-import com.physicaloid.lib.usb.driver.uart.ReadLisener;
-
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
-import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.microsoft.cognitiveservices.speech.CancellationDetails;
-import com.microsoft.cognitiveservices.speech.ResultReason;
-import com.microsoft.cognitiveservices.speech.SpeechConfig;
-import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult;
-import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+import com.microsoft.cognitiveservices.speech.*;
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
-
+import okhttp3.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Headers;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 import static android.Manifest.permission.INTERNET;
 import static android.Manifest.permission.RECORD_AUDIO;
@@ -72,8 +52,6 @@ public class MainActivity extends AppCompatActivity {
     SpellService spellService = new SpellService();
     @BindView(R.id.textViewMain)
     TextView textViewMain;
-
-    Physicaloid mPhysicaloid; // initialising library
 
     @BindView(R.id.spellTextView)
     TextView spellText;
@@ -115,18 +93,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-       /* getDetail();
-        try {
-            chanelUsb();
-        } catch (IOException e) {
-            textViewMain.setText(e.getMessage());
-            e.printStackTrace();
-        }*/
-        //getDetail();
         // Request permissions needed for speech recognition
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, INTERNET}, 5);
-
-        // create config
 
         try {
             speechConfig = SpeechConfig.fromSubscription(SpeechSubscriptionKey, SpeechRegion);
@@ -135,8 +103,6 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, ex.getMessage());
             return;
         }
-
-        findPort();
     }
 
     @Override
@@ -153,6 +119,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        findPort();
+        startIoManager();
+    }
+
     void findPort() {
         UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         final List<UsbSerialDriver> drivers =
@@ -161,9 +134,6 @@ public class MainActivity extends AppCompatActivity {
         final List<UsbSerialPort> result = new ArrayList<>();
         for (final UsbSerialDriver driver : drivers) {
             final List<UsbSerialPort> ports = driver.getPorts();
-
-            Log.d(TAG, String.format("+ %s: %s port%s",
-                    driver, Integer.valueOf(ports.size()), ports.size() == 1 ? "" : "s"));
 
             for(UsbSerialPort port : ports) {
                 Toast.makeText(this, "port: " + port.getPortNumber(), Toast.LENGTH_SHORT).show();
@@ -177,10 +147,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "port set!", Toast.LENGTH_SHORT).show();
         }
 
-        portChanged();
-    }
 
-    void portChanged() {
         if (sPort == null) {
             textViewMain.setText("No serial device.");
         } else {
@@ -195,15 +162,6 @@ public class MainActivity extends AppCompatActivity {
             try {
                 sPort.open(connection);
                 sPort.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-
-                /*showStatus(mDumpTextView, "CD  - Carrier Detect", sPort.getCD());
-                showStatus(mDumpTextView, "CTS - Clear To Send", sPort.getCTS());
-                showStatus(mDumpTextView, "DSR - Data Set Ready", sPort.getDSR());
-                showStatus(mDumpTextView, "DTR - Data Terminal Ready", sPort.getDTR());
-                showStatus(mDumpTextView, "DSR - Data Set Ready", sPort.getDSR());
-                showStatus(mDumpTextView, "RI  - Ring Indicator", sPort.getRI());
-                showStatus(mDumpTextView, "RTS - Request To Send", sPort.getRTS());*/
-
             } catch (IOException e) {
                 Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
                 textViewMain.setText("Error opening device: " + e.getMessage());
@@ -240,49 +198,6 @@ public class MainActivity extends AppCompatActivity {
         stopIoManager();
         startIoManager();
     }
-
-    /*void chanelUsb() throws IOException {
-        // Find all available drivers from attached devices.
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
-        if (availableDrivers.isEmpty()) {
-            textViewMain.setText("No drivers");
-            return;
-        }
-
-        // Open a connection to the first available driver.
-        UsbSerialDriver driver = availableDrivers.get(0);
-        final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        Intent usbApporval = new Intent();
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1337, usbApporval, 0);
-        usbManager.requestPermission(driver.getDevice(), pendingIntent);
-        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-        if (connection == null) {
-            // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
-            textViewMain.setText("Conn null");
-            return;
-        }
-
-        // Read some data! Most have just one port (port 0).
-        UsbSerialPort port = driver.getPorts().get(0);
-        try {
-            port.open(connection);
-            port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-
-            byte buffer[] = new byte[16];
-            port.read(buffer, 1000);
-            String msg = new String(buffer);
-            textViewMain.setText(textViewMain.getText()+msg);
-
-        } catch (IOException e) {
-            // Deal with error.
-            textViewMain.setText(e.getMessage());
-        } finally {
-            port.close();
-        }
-
-
-    }*/
 
     @OnClick(R.id.ButtonGet)
     void getSpell() {
@@ -369,37 +284,6 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, ex.getMessage());
         }
     }
-
-   /* public void getDetail() {
-        String actionString = this.getPackageName() + ".action.USB_PERMISSION";
-
-        PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new
-                Intent(actionString), 0);
-
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-
-        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
-        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
-        while (deviceIterator.hasNext()) {
-            UsbDevice device = deviceIterator.next();
-
-
-            manager.requestPermission(device, mPermissionIntent);
-            String Model = device.getDeviceName();
-
-            int DeviceID = device.getDeviceId();
-            int Vendor = device.getVendorId();
-            int Product = device.getProductId();
-            int Class = device.getDeviceClass();
-            int Subclass = device.getDeviceSubclass();
-
-            textViewMain.setText(textViewMain.getText() + "\nid " + DeviceID + ", vid " + Vendor + ",pid " + Product + ",class " + Class + ",sublcass " + Subclass);
-
-            Log.i("DEVICE", "id " + DeviceID + ", vid " + Vendor + ",pid " + Product + ",class " + Class + ",sublcass " + Subclass);
-
-
-        }
-    }*/
 
     public void updateReceivedData(byte[] data) {
         final String message = new String(data) + "\n";
