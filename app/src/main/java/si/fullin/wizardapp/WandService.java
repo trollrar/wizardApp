755210598928110606df18b8f2e86aab3b9975de
mihec <mihael.vercek@gmail.com>
2019-10-27 06:14:19 +0100

    Wand working

--- app/src/main/java/si/fullin/wizardapp/WandService.java
+++ app/src/main/java/si/fullin/wizardapp/WandService.java
@@ -1,12 +1,20 @@
 package si.fullin.wizardapp;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Handler;
 import android.icu.util.Measure;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.nimbusds.jose.util.Resource;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.lang.Character;
 
 /**
  * the main service that analyzes data from the wand, uses speech recog service and api service
@@ -21,8 +29,8 @@ public class WandService implements UsbSerialService.OnDataRecieved {
     ApiService apiService = new ApiService();
 
 
-
     ArrayList<Measurment> measurments = new ArrayList<>();
+
     public WandService(Activity activity, OnSpellCast onSpellCastListener, boolean enablePinger) {
         this.activity = activity;
         this.onSpellCastListener = onSpellCastListener;
@@ -63,110 +71,151 @@ public class WandService implements UsbSerialService.OnDataRecieved {
 
 
     ArrayList<ArrayList<Measurment>> list = new ArrayList<>();
-    int timerCounter=0;
+    int timerCounter = 0;
     boolean startTimerStarted = false;
     boolean stopTimerStarted = false;
 
     boolean logging = false;
 
+    byte[] currentStream = new byte[0];
+    private String spellName = "Failed";
+
+    boolean firstTimeSpell = true;
     @Override
     public void onDataReceived(byte[] data) {
-        activity.runOnUiThread(() ->
-                Toast.makeText(activity, "data came", Toast.LENGTH_SHORT).show()
-        );
-        // TODO
-        String dataStream = new String(data);
+        currentStream = data;
 
-        String[] split = dataStream.split("\r\n");
-        if (split.length>2) {
-            for (int i = 2; i < split.length-1; i++) {
-                String[] split1 = split[i].split(";");
-                if(split1.length==3){
-                    Measurment measurment;
-                    try {
-                        measurment = new Measurment(Double.valueOf(split1[0]), Double.valueOf(split1[1]), Double.valueOf(split1[2]));
-                    } catch (Exception ex) {
-                        continue;
+        String currentString = new String(data, StandardCharsets.UTF_8).replace("X", "");
+
+        String[] split1 = currentString.split(";");
+        if (split1.length == 3) {
+            Measurment measurment;
+            try {
+                measurment = new Measurment(Double.valueOf(split1[0]), Double.valueOf(split1[1]), Double.valueOf(split1[2]));
+            } catch (Exception ex) {
+                return;
+            }
+
+            if (!stopTimerStarted) {
+                if (measurment.x > 14000 && !startTimerStarted) {
+                    // Start stopwatch and measurement
+                    startTimerStarted = true;
+
+                } else if (measurment.x > 14000 && startTimerStarted) {
+                    // Start stopwatch and measurement
+                    timerCounter++;
+                } else if (measurment.x < 14000 && startTimerStarted) {
+                    // Start stopwatch and measurement
+                    startTimerStarted = false;
+                    timerCounter = 0;
+                }
+                if (timerCounter >= 2 && startTimerStarted) {
+                    // Start logging gestures.
+                    startTimerStarted = false;
+                    timerCounter = 0;
+                    logging = true;
+                    measurments.clear();
+
+                    if(firstTimeSpell) {
+                        firstTimeSpell = false;
+                        spellName = "Failed";
+                        speechService.listenForSpell(spellName -> {
+                            this.spellName = spellName;
+                        });
+
+                        activity.runOnUiThread(() ->
+                                {
+                                    TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                    viewById.setText("Listening....");
+
+                                }
+                        );
                     }
 
-                    if(!stopTimerStarted) {
-                        if (measurment.x > 14000 && !startTimerStarted) {
-                            /*activity.runOnUiThread(() ->
-                                    Toast.makeText(activity, "up started", Toast.LENGTH_SHORT).show()
-                            );*/
-                            // Start stopwatch and measurement
-                            startTimerStarted = true;
+                }
+            }
 
-                        } else if (measurment.x > 14000 && startTimerStarted) {
-                            // Start stopwatch and measurement
-                            timerCounter++;
-                        } else if (measurment.x < 14000 && startTimerStarted) {
-                            // Start stopwatch and measurement
-                            startTimerStarted = false;
-                            timerCounter = 0;
-                            /*activity.runOnUiThread(() ->
-                                    Toast.makeText(activity, "Failed up", Toast.LENGTH_SHORT).show()
-                            );*/
-                        }
-                        if (timerCounter > 100 && startTimerStarted) {
-                            // Start logging gestures.
-                            startTimerStarted = false;
-                            timerCounter = 0;
-                            logging = true;
-                            measurments.clear();
-                            /*activity.runOnUiThread(() ->
-                                    Toast.makeText(activity, "up ended", Toast.LENGTH_SHORT).show()
-                            );*/
-                        }
-                    }
+            if (logging) {
+                measurments.add(measurment);
+            }
 
-                    if(logging) {
-                        measurments.add(measurment);
-                    }
+            if (!startTimerStarted && logging) {
+                if (measurment.z > 14000 && !stopTimerStarted) {
+                    // Start stopwatch and measurement
+                    stopTimerStarted = true;
 
-                    if(!startTimerStarted && logging) {
-                        if (measurment.z > 14000 && !stopTimerStarted) {
-                            // Start stopwatch and measurement
-                            stopTimerStarted = true;
+                } else if (measurment.z > 14000 && stopTimerStarted) {
+                    // Start stopwatch and measurement
+                    timerCounter++;
+                } else if (measurment.z < 14000 && stopTimerStarted) {
 
-                            /*activity.runOnUiThread(() ->
-                                    Toast.makeText(activity, "stop started", Toast.LENGTH_SHORT).show()
-                            );*/
-                        } else if (measurment.z > 14000 && stopTimerStarted) {
-                            // Start stopwatch and measurement
-                            timerCounter++;
-                        } else if (measurment.z < 14000 && stopTimerStarted) {
-                            /*activity.runOnUiThread(() ->
-                                    Toast.makeText(activity, "stop failed", Toast.LENGTH_SHORT).show()
-                            );*/
-                            // Start stopwatch and measurement
-                            stopTimerStarted = false;
-                            timerCounter = 0;
-                        }
+                    // Start stopwatch and measurement
+                    stopTimerStarted = false;
+                    timerCounter = 0;
+                }
 
-                        if (timerCounter > 100 && stopTimerStarted) {
-                            // stop logging gestures.
-                            stopTimerStarted = false;
-                            timerCounter = 0;
-                            list.add(measurments);
-                            measurments = new ArrayList<>();
-                            // call onSpellCastListener.spellCast(boolean) when animations should be displayed
+                if (timerCounter >= 2 && stopTimerStarted) {
+                    // stop logging gestures.
+                    stopTimerStarted = false;
+                    timerCounter = 0;
+                    list.add(measurments);
 
-                            /*activity.runOnUiThread(() ->
-                                    Toast.makeText(activity, "Spell happened", Toast.LENGTH_SHORT).show()
-                            );*/
-                            logging = false;
-                        }
-                    }
+                    firstTimeSpell = true;
+                    logging = false;
+
+
 
                     activity.runOnUiThread(() ->
                             {
-                                TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
-                                viewById.setText("start " + startTimerStarted + " " + timerCounter + "\nstop " + stopTimerStarted + "\nlogging " + logging);
+                                Resources res = activity.getResources();
+                                if(spellName.equals(res.getStringArray(R.array.fire_atk)[0])) {
+                                    if (measurments.stream().anyMatch(x->x.x>13000)
+                                        &&measurments.stream().allMatch(y->y.y<13000)) {
+                                        TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                        viewById.setText(spellName);
+                                        onSpellCastListener.spellCast(true, spellName);
+                                    } else {
+                                        TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                        viewById.setText("Wrong Gesture");
+
+                                    }
+
+
+                                } else  if(spellName.equals(res.getStringArray(R.array.water_def)[0])) {
+                                    if (measurments.stream().anyMatch(x->x.y>13000)
+                                        &&measurments.stream().allMatch(y->y.x<13000)) {
+                                        TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                        viewById.setText(spellName);
+                                        onSpellCastListener.spellCast(true, spellName);
+                                    }else {
+                                        TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                        viewById.setText("Wrong Gesture");
+                                    }
+
+
+                                } else if(spellName.equals(res.getStringArray(R.array.plant_atk)[0])) {
+                                    if (measurments.stream().anyMatch(x -> x.y > 13000)
+                                            && measurments.stream().anyMatch(y -> y.x > 13000)) {
+                                        TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                        viewById.setText(spellName);
+                                        onSpellCastListener.spellCast(true, spellName);
+                                    }else {
+                                        TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                        viewById.setText("Wrong Gesture");
+                                    }
+                                } else {
+                                    TextView viewById = (TextView) activity.findViewById(R.id.textViewMain);
+                                    viewById.setText(spellName);
+                                    onSpellCastListener.spellCast(true, spellName);
+                                }
+                                spellName = "Failed";
+                                measurments = new ArrayList<>();
                             }
                     );
+
                 }
             }
+
 
 
 
@@ -192,6 +241,8 @@ public class WandService implements UsbSerialService.OnDataRecieved {
             this.y = y;
             this.z = z;
         }
+
+
 
         @Override
         public String toString() {
